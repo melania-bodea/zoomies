510ba3b0763f4428afb606aaab4827b45b72e382
Neamt Tudor <neamt.tudor14@yahoo.com>
2026-04-27 21:36:36 +0300

    Introductory Code
    Added Introductory Code and Mode API

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Zoomies.Models;
+
+namespace Zoomies.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Zoomies.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
